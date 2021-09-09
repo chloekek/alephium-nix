883b3ea1c23ffbf833023b2aae8a4b98e7da2c99
Chloé Kekoa <chloekek@use.startmail.com>
2021-09-09 19:23:09 +0200

    Add enough configurability to run testnet node

--- nixos-module.nix
+++ nixos-module.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, ... }:
 
+let
+
+    # Convenient way to reference the Alephium config.
+    cfg = config.services.alephium;
+
+in
+
 {
     options.services.alephium = {
 
@@ -21,18 +28,34 @@
             '';
         };
 
-        home = lib.mkOption {
-            type = lib.types.path;
-            default = "/var/lib/alephium";
+        discovery.bootstrap = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
             description = ''
-                Alephium home directory which stores
-                configuration and the blockchain.
+                Addresses of other Alephium nodes to connect to initially.
+                This is different for each network, so there is no default.
+            '';
+        };
+
+        mining.miner-addresses = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [];
+            description = ''
+                List of addresses to which mining rewards are to be deposited.
+                You can leave this empty if you do not participate in mining.
+            '';
+        };
+
+        network.network-id = lib.mkOption {
+            type = lib.types.int;
+            description = ''
+                Alephium network identifier.
+                Set to 1 for testnet.
             '';
         };
 
     };
 
-    config = lib.mkIf config.services.alephium.enable {
+    config = lib.mkIf cfg.enable {
 
         users.users.alephium = {
             group = "alephium";
@@ -43,25 +66,66 @@
         users.groups.alephium = {
         };
 
-        systemd.services.alephium = {
+        environment.etc."alephium/user.conf" = {
+            text = ''
+                alephium.mining.miner-addresses = ${
+                    builtins.toJSON cfg.mining.miner-addresses
+                }
+                alephium.network.network-id = ${
+                    toString cfg.network.network-id
+                }
+                alephium.discovery.bootstrap = ${
+                    builtins.toJSON cfg.discovery.bootstrap
+                }
+                alephium.wallet.secret-dir = /var/lib/alephium/wallet
 
+                # TODO: Make the remaining options configurable.
+                # alephium.network.external-address = "x.x.x.x:9973"
+            '';
+        };
+
+        systemd.services.alephium = {
             description = "Alephium node";
+
+            # TODO: Configure service to restart when config file has changed?
 
             after = [ "network.target" ];
             wantedBy = [ "multi-user.target" ];
 
             environment = {
-                ALEPHIUM_HOME = config.services.alephium.home;
+                # This is where Alephium node stores config and blockchain.
+                ALEPHIUM_HOME = "/var/lib/alephium/home";
             };
 
             serviceConfig = {
                 User = "alephium";
                 Group = "alephium";
-                ExecStart = "${config.services.alephium.package}/bin/alephium";
+                ExecStart = "${cfg.package}/bin/alephium";
+
+                # systemd prepends /var/lib to this.
+                StateDirectory = "alephium";
+
                 Restart = "always";
                 TimeoutSec = 5;
             };
 
+            preStart = ''
+                home=/var/lib/alephium/home
+                user_conf=$home/user.conf
+
+                # Create the Alephium home directory.
+                # systemd will have created its parent already.
+                if ! [[ -e $home ]]; then
+                    mkdir "$home"
+                fi
+
+                # Alephium configuration file is in ALEPHIUM_HOME,
+                # but we want to store it in /etc/alephium.
+                # Using a symbolic link for this works.
+                if ! [[ -e $user_conf ]]; then
+                    ln --symbolic /etc/alephium/user.conf "$user_conf"
+                fi
+            '';
         };
 
     };
