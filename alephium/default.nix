b5ea00691222d752c07d7077254a1390e650419e
Chloé Kekoa <chloekek@use.startmail.com>
2021-10-22 01:27:10 +0200

    alephium: 0.11.1 → 0.11.3

--- alephium/default.nix
+++ alephium/default.nix
@@ -2,11 +2,11 @@
 
 stdenvNoCC.mkDerivation rec {
     pname = "alephium";
-    version = "0.11.1";
+    version = "0.11.3";
 
     src = fetchurl {
         url = "https://github.com/alephium/alephium/releases/download/v${version}/alephium-${version}.jar";
-        sha256 = "1dpr4x83kw4ghy28zhnlnimz0kc0qfb5lngx8ivnbffzj4yla5zx";
+        sha256 = "0pdx3jx9sm41k5m92jwdgqm4ig07ga3pvr6ca2cvgn9h5k3kkmc7";
     };
 
     buildInputs = [
